be16a6b9070e9d08cf158b6d6367f0151e56d929
vaibhave <vaibhavesvaibahve@gmail.com>
2020-09-21 20:34:12 +0530

    Cleaned reverse palindrome

--- Python/reverse palindrome.py
+++ Python/reverse palindrome.py
@@ -2,22 +2,22 @@
 Python Programe To Check if  a String is Palindrome or Not
 '''
 
-check1 = input("Choose Your Input For Check Palindrome\n For Integer Type --> i \n For Word Type   -->s\n").lower()
-for i range(1,3):
-	if check1 == 's':
-		x = input("Enter Your Word Here :\n").lower()
-	elif check1 == 'i':
-		x = input("Enter Your Number Here :\n")
-	else:
-		print("Please Enter I or S only")
+for i in range(1,3):
+    check1 = input("Choose Your Input For Check Palindrome\n For Integer Type --> i \n For Word Type   -->s\n").lower()
+    if check1 == 's':
+        x = input("Enter Your Word Here :\n").lower()
+    elif check1 == 'i':
+        x = input("Enter Your Number Here :\n")
+    else:
+        print("Please Enter I or S only")
+        break
+    reverse = "" # Empty String to save Reverse value in it.
 
-	w = "" # Empty String to save Reverse value in it.
-
-#save string x to a new new variable and compare them
-	for i in x:
-		w = i + w   # save new value to w 
-		if (x==w):
-			print("Yes")
-			break
-	else:
-		print("No")
+    #Construct the reverse value/string 
+    for i in x:
+        reverse = i + reverse   
+    #Compare the string and its reverse 
+    if (x==reverse):
+        print("Yes")
+    else:
+        print("No")
