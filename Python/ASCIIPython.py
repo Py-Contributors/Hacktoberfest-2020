af2880073dd22dd812642c518db3ed4b94c2026a
AmaaniGoose <amanullah1707@gmail.com>
2020-09-28 18:51:47 +0530

    Create ASCIIPython.py

---
+++ Python/ASCIIPython.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# Python program to print  
+# ASCII Value of Character 
+
+c=input("Enter the character whose ASCII code you want: ")
+
+# print the ASCII value of assigned character in c 
+print("The ASCII value of '" + c + "' is", ord(c))
+
+input("Press enter to exit ;)")
+
+
+# In[ ]:
+
+
+
+
