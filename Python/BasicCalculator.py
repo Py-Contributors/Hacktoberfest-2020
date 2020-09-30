3fccc4845fea0e2ba5dd027612253521b1a192f3
Md Shams Tabrej Ansari <rajansari66789@gmail.com>
2020-10-01 01:38:47 +0530

    Added some more operation
    Added operator like  Square, Power and Square root.

--- Python/BasicCalculator.py
+++ Python/BasicCalculator.py
@@ -16,14 +16,26 @@ def div(num1, num2):
 		exit()
 	return num1 / num2
 
+def squ(num1):
+	return num1 ** 2
+
+def powe(num1 , num2):
+	return num1 ** num2
+
+def sqroot(num1):
+	return num1 ** 0.5
+
 print("Please Select Operations\n "\
 	   "1. Addition\n " 
 	   "2. Subtraction \n"
 	   "3. Multiplication \n"
-	   "4. Divide \n" )
+	   "4. Divide \n"
+           "5. Square \n"
+           "6. Power \n"
+           "7. Square root \n")
 
 #Take Input From Keyword
-select = input("Press\n" "1 " "2 " "3 " "4 \n")
+select = input("Press\n" "1 " "2 " "3 " "4 " "6 " "7 \n")
 
 number_1 = int(input("Type Your First Number :"))
 number_2 = int(input("Type Your Second Number :"))
@@ -44,6 +56,18 @@ elif select == '3':
 elif select == '4':
 	print(number_1, '/', number_2, '=')
 	print(div(number_1, number_2))
+	
+elif select == '5':
+	print(number_1, '^', 2'=')
+	print(squ(number_1))
+	
+elif select == '6':
+	print(number_1, '^',number_2, '=')
+	print(powe(number_1, number_2))
+	
+elif select == '7':
+	print(number_1, 'sq root','=')
+	print(sqroot(number_1))
 
 else:
 	print('Invalid Input ! Try aagian ')
