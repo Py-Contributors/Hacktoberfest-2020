db1946ef708c65e02556ebf63c907d87379baa02
Ameya Jain <ameiya.jain@hotmail.com>
2020-10-01 00:07:49 +0530

    Final Commit

--- Python/autoCorrectWord.py
+++ Python/autoCorrectWord.py
@@ -1,5 +1,13 @@
+#Auto Correcting the given word.
+
+#Use "pip install textblob" to install the package textblob.
+
+#imports the textblob library
 from textblob import TextBlob
 
-a = "Pogrram"
-b = TextBlob(a)
-print(b.correct())
+#We can put the word or sentence in variable or take it as an input.
+#E.g: word = "Pogrram"
+word = input("Enter the String: ")
+
+correctWord = TextBlob(word)
+print("The correct String is: ", correctWord.correct())
