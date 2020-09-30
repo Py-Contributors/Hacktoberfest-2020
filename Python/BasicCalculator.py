08a5094438dbcf6f567ca263c7dc2594c24e4ea3
Triben <daniealtribs@gmail.com>
2020-10-01 02:06:21 +0530

    added new feature- calculator can be used many times without runnig again

--- Python/BasicCalculator.py
+++ Python/BasicCalculator.py
@@ -1,51 +1,28 @@
-#Basic Calculator in Python without any gui.
-
-#create functions for calculations
-def add(num1, num2):
-	return num1 + num2
-
-def sub(num1, num2):
-	return num1 - num2
-
-def mul(num1, num2):
-	return num1 * num2
-
-def div(num1, num2):
-	if num2 == 0:
-		print("Can't divide a number by 0")
-		exit()
-	return num1 / num2
-
-print("Please Select Operations\n "\
-	   "1. Addition\n " 
-	   "2. Subtraction \n"
-	   "3. Multiplication \n"
-	   "4. Divide \n" )
-
-#Take Input From Keyword
-select = input("Press\n" "1 " "2 " "3 " "4 \n")
-
-number_1 = int(input("Type Your First Number :"))
-number_2 = int(input("Type Your Second Number :"))
-
-
-if select == '1':
-	print(number_1, "+", number_2, '=')
-	print(add(number_1, number_2))
-
-elif select == '2':
-	print(number_1, '-', number_2, '=')
-	print(sub(number_1, number_2))
-
-elif select == '3':
-	print(number_1, '*', number_2, '=')
-	print(mul(number_1, number_2))
-
-elif select == '4':
-	print(number_1, '/', number_2, '=')
-	print(div(number_1, number_2))
-
-else:
-	print('Invalid Input ! Try aagian ')
-	
-print('Thanks for Using Calculator')
+class Calculator:
+    def addition(self,x,y):
+        return x+y
+    def subtraction(self,x,y):
+        return x-y
+    def multiplication(self,x,y):
+        return x*y
+    def division(self,x,y):
+        return x/y
+c=Calculator()
+flag="Y"
+while flag=="Y" or flag=="y":
+    print("1)Addition\n2)Subtraction\n3)Multiplication\n4)Division\n")
+    choice=int(input("Enter the operator : "))
+    a,b=input("Enter two operands seperated by space : ").split()
+    a=int(a)
+    b=int(b)
+    if choice==1:
+        print(f"Sum is {c.addition(a,b)}")
+    elif choice==2:
+        print(f"Difference is {c.subtraction(a,b)}")
+    elif choice==3:
+        print(f"Product is {c.multiplication(a,b)}")
+    elif choice==4:
+        print(f"Division is {c.division(a,b)}")
+    else:
+        print("Please enter correct input")
+    flag=input("\nTo calculate again enter 'Y' or enter any key to exit : ")
