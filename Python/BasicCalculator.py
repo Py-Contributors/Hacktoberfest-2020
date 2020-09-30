1c9349f1709d3683f9b3158e8dbc5bbb34b2abb5
Ritika Jaiswal <61064446+18-Ritika@users.noreply.github.com>
2020-10-01 02:53:55 +0530

    Update BasicCalculator.py

--- Python/BasicCalculator.py
+++ Python/BasicCalculator.py
@@ -15,12 +15,27 @@ def div(num1, num2):
 		print("Can't divide a number by 0")
 		exit()
 	return num1 / num2
+# this power function is contributed by Ritika Jaiswal.
+def power(num1, num2): 
+	if(num2 == 0): return 1
+	temp = power(num1, int(num2 / 2)) 
+	if (num2 % 2 == 0): 
+		return temp * temp 
+	else: 
+		if(num2 > 0): return x * temp * temp 
+		else: return (temp * temp) / num1 
+		
+
+# This code is contributed by Smitha Dinesh Semwal. 
+ 
 
 print("Please Select Operations\n "\
 	   "1. Addition\n " 
 	   "2. Subtraction \n"
 	   "3. Multiplication \n"
-	   "4. Divide \n" )
+	   "4. Divide \n"
+           "5. Power \n"   
+     )
 
 #Take Input From Keyword
 select = input("Press\n" "1 " "2 " "3 " "4 \n")
@@ -44,6 +59,10 @@ elif select == '3':
 elif select == '4':
 	print(number_1, '/', number_2, '=')
 	print(div(number_1, number_2))
+	
+elif select == '5':
+	print(number_1, '^', number_2, '=')
+	print(power(number_1, number_2))
 
 else:
 	print('Invalid Input ! Try aagian ')
