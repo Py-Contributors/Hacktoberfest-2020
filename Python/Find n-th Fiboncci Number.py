0ed4882a06d9fbf8dd04990cc498fd024c1ed53f
jg7 <jatin_gupta7@yahoo.co.in>
2020-10-01 18:17:21 +0530

    dynamic programming for fibonacci

--- Python/Find n-th Fiboncci Number.py
+++ Python/Find n-th Fiboncci Number.py
@@ -1,16 +1,23 @@
 '''
 Fibonacci Series start from 1. it makes new number by adding previous 2 Numbers.
 1,1,2,3,5
+
+Make a array storing the series of fibonacci number
+if the nth number of fibonacci is present then the answer is printed 
+else it computes and stores it in the array
 '''
+
+fibArr = [0,1]
+
 def Fibonacci(n):
 	if n<0:
 		print("Wrong Input ! Please Check Again")
-	elif n == 1:
-		return 0
-	elif n == 2:
-		return 1
+	elif n <= len(fibArr):
+		return fibArr[n-1]
 	else:
-		return Fibonacci(n-1) + Fibonacci(n-2)
+		temp = Fibonacci(n-1) + Fibonacci(n-2)		
+		fibArr.append(temp)		
+		return temp
 
 
 print(Fibonacci(9))
