7c4de34a22e81550ba15bd76158747f6fa06fa23
suraj Nayak <surajnayakmuniyal@gmail.com>
2020-10-03 11:24:54 +0530

    Add python file

---
+++ Python/palindrome.py
@@ -0,0 +1,7 @@
+p_phrase = "was it a car or a cat I saw"
+r_phrase=p_phrase[::-1]
+print(r_phrase)
+if r_phrase==p_phrase:
+    print("string is palindrome")
+else:
+    print("string is nor palindrome")
