d9d63a916a660fdf44dad4f11f66b7f6a06b596c
Rishi-Sharma-2027 <65058507+Rishi-Sharma-2027@users.noreply.github.com>
2020-10-04 20:49:46 +0530

    Update Find n-th Fiboncci Number.py
    did the change on line no - 6

--- Python/Find n-th Fiboncci Number.py
+++ Python/Find n-th Fiboncci Number.py
@@ -3,15 +3,14 @@ Fibonacci Series start from 1. it makes new number by adding previous 2 Numbers.
 1,1,2,3,5
 '''
 def Fibonacci(n):
-	if n<0:
-		print("Wrong Input ! Please Check Again")
+	if n <= 0:
+		print("Wrong Input ! Please Try Again")
 	elif n == 1:
 		return 0
 	elif n == 2:
 		return 1
 	else:
-		return Fibonacci(n-1) + Fibonacci(n-2)
-
+		return Fibonacci(n-1) + Fibonacci(n-2) 
 
 print(Fibonacci(9))
 '''so in Fibonacci Series (9th ) number is 21'''
