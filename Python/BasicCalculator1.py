7849dd68e5ba1250970462b3223e7fe60ccf7765
kevin454475 <71903670+kevin454475@users.noreply.github.com>
2020-10-05 13:31:47 -0400

    Replace If/Else chain
    - Made blank space at  start of operations list consistent
    - Changed if/elif flow to a dict.get flow
    - Used eval to pull correct operation reducing code duplication

--- Python/BasicCalculator1.py
+++ Python/BasicCalculator1.py
@@ -30,6 +30,7 @@ number_2 = int(input("Type Your Second Number :"))
 
 options = {"1": ("add","+"), "2":("sub","-"), "3":("mul","*"), "4":("div","/")}
 selected_op = options.get(select,False)
+print(selected_op)
 
 if not selected_op:
     print("Invalid Input! Try again")
@@ -37,4 +38,30 @@ else:
     print(f"{number_1} {selected_op[1]} {number_2} = ")
     print(eval(f'{selected_op[0]}({number_1}, {number_2})'))
 
+
+#res = eval(f'{options[select]}({number_1},{number_2})') if selected_op\
+      #else "Invalid Input! Try again"
+      
+#print(res)
+
+"""
+if select == '1':
+	print(number_1, "+", number_2, '=')
+	print(add(number_1, number_2))
+
+elif select == '2':
+	print(number_1, '-', number_2, '=')
+	print(sub(number_1, number_2))
+
+elif select == '3':
+	print(number_1, '*', number_2, '=')
+	print(mul(number_1, number_2))
+
+elif select == '4':
+	print(number_1, '/', number_2, '=')
+	print(div(number_1, number_2))
+
+else:
+	print('Invalid Input ! Try aagian ')
+"""
 print('Thanks for Using Calculator')
