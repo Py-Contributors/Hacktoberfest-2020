19d1501f1518961b5ddf51595886737424b2968c
guilhermeaureliano <guilhermeaurelianoec@gmail.com>
2020-10-06 14:13:32 -0300

    Bubble Sorte in Java

---
+++ Java/BubbleSort.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BubbleSort {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Type the size of the Array: ");
+		int x = input.nextInt();
+		
+		boolean sorted = true;
+		int[] array = new int[x];
+		
+		// filling the array
+		for(int i = 0; i < array.length; i++) {
+			int a = input.nextInt();
+			array[i] = a;
+		}
+		
+		// sort the array
+		int aux = 0;
+		for(int i = 0; i < array.length; i++) {
+			for (int j = 0; j < (array.length - 1); j ++) {
+				if (array[j] > array[j + 1]) {
+					aux = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = aux;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(array));
+	}
+}
