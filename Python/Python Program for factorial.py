769b9b003fdcdf93ed186853b81c97556d32c56f
Mohd Shafahad <42795017+mohdshafahad@users.noreply.github.com>
2020-10-16 15:43:39 +0530

    Update Python Program for factorial.py

--- Python/Python Program for factorial.py
+++ Python/Python Program for factorial.py
@@ -6,10 +6,13 @@ n = n*(n-1)*(n-2)*(n-3).......3*2*1
 
 '''
 def factorial(n):
-	#Line to Find Factorial
-	return 1 if (n==1 or n==0) else n * factorial(n -1);
+    #Line to Find Factorial
+    if (n==1 or n==0):
+            return 1
+    else:
+        return (n * factorial(n-1))
 
 	#Driver Code
 num = int(input(" Enter Number For Factorial :\n"))
 answer = factorial(num)
-print(f"Factorial of {num} is {answer}.")
+print("Factorial of "+str(num)+" is ",answer)
