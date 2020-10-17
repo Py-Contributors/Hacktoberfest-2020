af32a11cc91e8a1457b1ecf51fbe8812708f721d
tejaswari7 <tejaswaribhamare@gmail.com>
2020-10-17 11:12:32 +0530

    Update Basic Calculator
    Updated Calculator with Exception Handling and made some changes

--- Python/BasicCalculator.py
+++ Python/BasicCalculator.py
@@ -1,51 +1,25 @@
-#Basic Calculator in Python without any gui.
-
-#create functions for calculations
-def add(num1, num2):
-	return num1 + num2
-
-def sub(num1, num2):
-	return num1 - num2
-
-def mul(num1, num2):
-	return num1 * num2
-
-def div(num1, num2):
-	if num2 == 0:
-		print("Can't divide a number by 0")
-		exit()
-	return num1 / num2
-
-print("Please Select Operations\n "\
-	   "1. Addition\n " 
-	   "2. Subtraction \n"
-	   "3. Multiplication \n"
-	   "4. Divide \n" )
-
-#Take Input From Keyword
-select = input("Press\n" "1 " "2 " "3 " "4 \n")
-
-number_1 = int(input("Type Your First Number :"))
-number_2 = int(input("Type Your Second Number :"))
-
-
-if select == '1':
-	print(number_1, "+", number_2, '=')
-	print(add(number_1, number_2))
-
-elif select == '2':
-	print(number_1, '-', number_2, '=')
-	print(sub(number_1, number_2))
-
-elif select == '3':
-	print(number_1, '*', number_2, '=')
-	print(mul(number_1, number_2))
-
-elif select == '4':
-	print(number_1, '/', number_2, '=')
-	print(div(number_1, number_2))
-
-else:
-	print('Invalid Input ! Try aagian ')
-	
-print('Thanks for Using Calculator')
+# Selection of operation
+try:
+	while True:
+		choice = int(input("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Modular\n6.Exit\nEnter choice: "))
+		if(choice == 6):
+			break
+		num1 = float(input("Enter first number: "))
+		num2 = float(input("Enter second number: "))
+       	# Check which operation to perform 
+		if choice == 1:
+			print(num1, "+", num2, "=", num1+num2)
+		elif choice == 2:
+			print(num1, "-", num2, "=", num1-num2)
+		elif choice == 3:
+			print(num1, "*", num2, "=", num1*num2)
+		elif choice == 4:
+			print(num1, "/", num2, "=", num1/num2)
+		elif choice == 5:
+			print(num1, "%", num2, "=", num1%num2)
+		else:
+			print("Invalid Choice")
+except ZeroDivisionError:
+	print("Denominator cannot be 0")
+except ValueError:
+	print("Enter Numbers only")
