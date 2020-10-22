1148ce8056011992a9872e4c3fc26de873dee2b5
dexcoder7 <dexcoder7@gmail.com>
2020-10-22 22:21:33 +0500

    Doc : Improvemnets in code

--- Python/BasicCalculator.py
+++ Python/BasicCalculator.py
@@ -1,20 +1,20 @@
 #Basic Calculator in Python without any gui.
 
 #create functions for calculations
-def add(num1, num2):
-	return num1 + num2
+def add(number1, number2):
+	return number1 + number2
 
-def sub(num1, num2):
-	return num1 - num2
+def sub(number1, number2):
+	return number1 - number2
 
-def mul(num1, num2):
-	return num1 * num2
+def mul(number1, number2):
+	return number1 * num2
 
-def div(num1, num2):
+def div(number1, number2):
 	if num2 == 0:
 		print("Can't divide a number by 0")
 		exit()
-	return num1 / num2
+	return number1 / number2
 
 print("Please Select Operations\n "\
 	   "1. Addition\n " 
@@ -25,8 +25,8 @@ print("Please Select Operations\n "\
 #Take Input From Keyword
 select = input("Press\n" "1 " "2 " "3 " "4 \n")
 
-number_1 = int(input("Type Your First Number :"))
-number_2 = int(input("Type Your Second Number :"))
+number_1 = int(input("Type Your First Number Here:"))
+number_2 = int(input("Type Your Second Number Here:"))
 
 
 if select == '1':
