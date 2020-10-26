ef5dcdfb12ab9945f912e68169de323f15010e8a
casiimir <cp.ciancimino@gmail.com>
2020-10-26 20:36:55 +0100

    Add Python random_data script

--- Python/random_data.py
+++ Python/random_data.py
@@ -1,12 +1,12 @@
 # Import the random Library
 import random 
 
-# function that return a random element
+# Function that return a random element
 def random_data (data_list):
   return ("Random element =>",random.choice(data_list))
 
-# select a dataset or make one, example an array of elements
+# Select a dataset or make one, example an array of elements
 data_list = ['apple', 'kiwi', 'banana', 'orange', 'mango', 'strawberry', 'watermelon', 'pear']
 
-# example of use:
+# Example of use:
 random_data (data_list)
